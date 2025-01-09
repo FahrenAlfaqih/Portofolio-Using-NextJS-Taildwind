9176b41e039c04d0f810ebf854874621d5a2737e
Fahren Alfaqih <fahren66@gmail.com>
2025-01-09 15:30:47 +0700

    Grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-import Grid from "@/components/Grid";
+"use client";
+
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome, FaTable } from "react-icons/fa";
-import { FaPerson } from "react-icons/fa6";
+import Grid from "@/components/Grid";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-      <FloatingNav navItems={[
-            {name: 'Home', link: "/", icon: <FaHome /> },
-            {name: 'About', link: "/about", icon: <FaPerson /> },
-            {name: 'Project', link: "/project", icon: <FaTable /> }
-          ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
       </div>
     </main>
-
   );
-}
+};
+
+export default Home;
